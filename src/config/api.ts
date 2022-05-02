620c860a544b1cd37083f64450b1787b443f1d08
Nytrox-x <m47heusdr@protonmail.com>
2022-05-02 12:02:47 -0300

    Heroku

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,9 @@
-export default(): {
-   API_PORT: number;
-   API_BASE_URL: string;
+export default (): {
+  API_PORT: number;
+  API_BASE_URL: string;
 } => {
-  return  {
-    API_PORT: Number(process.env.API_PORT) ?? 3000,
-    API_BASE_URL: process.env.API_BASE_URL ?? '/v1/fub'
-  }
-}
+  return {
+    API_PORT: Number(process.env.PORT) ?? 3000,
+    API_BASE_URL: process.env.API_BASE_URL ?? '/v1/fub',
+  };
+};
