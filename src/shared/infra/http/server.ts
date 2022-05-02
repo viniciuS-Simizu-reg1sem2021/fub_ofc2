44a17debed5fde9bd5c508164731ef4215fd17fb
Nytrox-x <m47heusdr@protonmail.com>
2022-05-02 11:33:32 -0300

    Trying to fix prod

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,7 +3,7 @@ import { app } from '@shared/infra/http/app';
 
 const apiConfig = api();
 
-app.listen(apiConfig.API_PORT, async () => {
+app.listen(apiConfig.API_PORT || 3000, async () => {
   console.log(`SERVER STARTED ON http://localhost:${apiConfig.API_PORT}`);
 });
 
